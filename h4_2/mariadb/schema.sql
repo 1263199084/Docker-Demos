4ab9cb6d49784f1f82134fa6a5d3cd09e15483d2
qyanzh <qyanzh@gmail.com>
2020-05-11 11:11:14 +0800

    add depends_on

--- h4_2/mariadb/schema.sql
+++ h4_2/mariadb/schema.sql
@@ -1,9 +1,8 @@
 create database db_example;
-
 -- Creates the new database
+
 create user 'springuser' @'%' identified by 'ThePassword';
-
 -- Creates the user
-grant all on db_example.* to 'springuser' @'%';
 
+grant all on db_example.* to 'springuser' @'%';
 -- Gives all privileges to the new user on the newly created database
